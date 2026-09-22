e5107d977971c58a6d67ce2970925fc90b30be6e
Gabriel Hernández <73655786+svpirate503@users.noreply.github.com>
2026-09-22 17:21:27 -0400

    dotenv

--- leoyangel/settings.py
+++ leoyangel/settings.py
@@ -15,10 +15,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+from dotenv import load_dotenv
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+load_dotenv(BASE_DIR / ".env")  # pon esta línea después de donde se define BASE_DIR
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "clave-solo-para-local")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
